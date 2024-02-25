95d34d8b2fa7909001e3f36fbd7de98b240ec66f
Danijela Vukosav <danijela99vukosav@gmail.com>
2024-02-25 16:28:34 +0100

    remove(final): project
    - Move project to another repository

--- final/src/services/httpCommon.ts
+++
@@ -1,9 +0,0 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from "axios";
-
-// @ts-ignore
-export const http: AxiosInstance = axios.create({
-    "baseURL": 'http://localhost:8080', // Zamijenite s vašom URL adresom
-    // "headers": {
-    //     'Content-Type': 'multipart/form-data',
-    // },
-});
